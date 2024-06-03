d42b66673057a8541d1bbbc47f5d75b19cd1c1ce
Omar J <100746749+omsinj@users.noreply.github.com>
2024-06-03 22:32:04 +0100

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,30 @@
+int main () 
+{
+int test_scores [] {4,10,6,9,50,21};
+cout << "First score at index 0: " << test_scores[0] << endl;
+    
+    
+char vowels[] {'a','e','i','o','u'};
+cout << "\nSecond vowel is: " << vowels[1] << endl;
+
+double hi_temps[] {90.1,89.8,77.5,81.6};
+
+cout << "\nThe first high temperature is: " << hi_temps[0] <<endl;
+cout << "\nThe second high temperature is: " << hi_temps[1] <<endl;
+cout << "\nThe third high temperature is: " << hi_temps[2] <<endl;
+
+  cout << "\n Enter three test scores: ";
+int test [] {};
+cin >> test[0];
+cin >> test[1];
+cin >> test[2];
+
+cout << "\n test scores are now: " << endl;
+cout << "\n1st score is: " << test[0];
+cout << "\nsecond score is: " << test[1];
+cout << "\nthird score is: " << test[2];
+
+cout << endl;
+
+return 0;
+}
